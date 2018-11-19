cd841c50224a6a9535e5cd0968c89d870b30d00b
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-19 19:09:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,45 @@
+
+// add
+function add (a, b) {
+  return a + b
+}
+
+//subtract
+
+function subtract (a, b) {
+  return a - b
+}
+
+//multiply
+
+function multiply (a, b) {
+  return a * b
+}
+
+//divide
+
+function divide (a, b) {
+  return a / b
+}
+
+//increment
+
+function inc (n) {
+  n++
+  return n
+}
+
+//decrements
+
+function dec (n) {
+  n--
+  return n
+}
+
+//parsing n
+
+function makeInt (n) {
+  return n
+}
+
+//parsing with base 10
